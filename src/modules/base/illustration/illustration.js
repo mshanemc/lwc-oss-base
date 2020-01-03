56853840cff09a8f7bd397ff7044fac0fc8a73cd
mshanemc <shane.mclaughlin@salesforce.com>
2020-01-03 11:08:05 -0600

    base components from deploy-to

--- src/modules/base/illustration/illustration.js
+++ src/modules/base/illustration/illustration.js
@@ -1,22 +1,22 @@
 import { LightningElement, api } from 'lwc';
 
 export default class Illustration extends LightningElement {
-  @api variant;
-  @api size = 'small';
-  @api heading;
+    @api variant;
+    @api size = 'small';
+    @api heading;
 
-  get isWalkthroughNotAvailable() {
-    return this.variant === 'WalkthroughNotAvailable';
-  }
-
-  get isPageNotAvailable() {
-    return this.variant === 'PageNotAvailable';
-  }
-
-  get sizeClass() {
-    if (this.size === 'large') {
-      return `slds-illustration slds-illustration_large`;
+    get isWalkthroughNotAvailable() {
+        return this.variant === 'WalkthroughNotAvailable';
     }
-    return `slds-illustration slds-illustration_small`;
-  }
+
+    get isPageNotAvailable() {
+        return this.variant === 'PageNotAvailable';
+    }
+
+    get sizeClass() {
+        if (this.size === 'large') {
+            return `slds-illustration slds-illustration_large`;
+        }
+        return `slds-illustration slds-illustration_small`;
+    }
 }
